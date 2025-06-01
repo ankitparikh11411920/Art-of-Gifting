6c15c87c2c7fedcc74da34d2a7b427585428ecbd
PAIN <madara.uchiha.mu.mu199@gmail.com>
2025-06-01 15:18:25 +0530

    key change

--- user/src/main/java/com/example/artofgifting/Notify/APIService.java
+++ user/src/main/java/com/example/artofgifting/Notify/APIService.java
@@ -1,5 +1,7 @@
 package com.example.artofgifting.Notify;
 
+import android.os.Environment;
+import com.example.artofgifting.BuildConfig;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Headers;
@@ -9,7 +11,7 @@ public interface APIService {
     @Headers(
             {
                     "Content-Type:application/json",
-                    "Authorization:key=AAAAXzAAmBk:APA91bHmbPJBjG9y4wLpLSyHZ1BK7wpXVI3KBUj9xpFgTHWGFwdT44fgpbS7q1LA2Q4Q5RH5Ka6tJerM-wb2GwJ9Bw7EgLItcF2yPBSc0ziJZlWXbSfx1if2ZzU89P9ovSb2sl9BT0sB" // Your server key refer to video for finding your server key
+                    "Authorization:key="+ BuildConfig.FIREBASE_MESSAGING_KEY // Your server key refer to video for finding your server key
             }
     )
 
